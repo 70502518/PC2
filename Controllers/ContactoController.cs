cbd80553b24a8e6a4689b7ec605b51072501fde9
Kelly PC <kelly_pena@usmp.pe>
2020-06-23 19:32:55 -0500

    .

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,17 @@
+
+public class ContactoController : Controller
+{
+    public IActionResult NuevoCOntacto()
+    {
+      //TODO: Implement Realistic Implementation
+      return View();
+    }
+
+
+    [HttPost]
+    public IActionResult NuevoContacto(String nombre, string contraseña)
+    {
+      //TODO: Implement Realistic Implementation
+      return View();
+    }
+}
